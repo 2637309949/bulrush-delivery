4f04211fedb84d0944569da5a6d46e19e2b9457c
double <2637309949@qq.com>
2019-07-02 22:37:29 +0800

    feat: 架构调整

--- application.go
+++ application.go
@@ -12,13 +12,11 @@ import (
 	"net/http"
 	"path"
 
-	"github.com/2637309949/bulrush"
 	"github.com/gin-gonic/gin"
 )
 
 // Delivery service static files
 type Delivery struct {
-	bulrush.PNBase
 	Path      string
 	URLPrefix string
 }
@@ -33,18 +31,16 @@ func New() *Delivery {
 }
 
 // Plugin for gin
-func (delivery *Delivery) Plugin() interface{} {
-	return func(httpProxy *gin.Engine) {
-		lf := localFile(delivery.Path, false)
-		fileserver := http.FileServer(lf)
-		if delivery.URLPrefix != "" {
-			fileserver = http.StripPrefix(delivery.URLPrefix, fileserver)
-		}
-		httpProxy.GET(delivery.URLPrefix+"/*any", func(c *gin.Context) {
-			if lf.Exists(delivery.URLPrefix, c.Request.URL.Path) {
-				fileserver.ServeHTTP(c.Writer, c.Request)
-				c.Abort()
-			}
-		})
+func (delivery *Delivery) Plugin(httpProxy *gin.Engine) {
+	lf := localFile(delivery.Path, false)
+	fileserver := http.FileServer(lf)
+	if delivery.URLPrefix != "" {
+		fileserver = http.StripPrefix(delivery.URLPrefix, fileserver)
 	}
+	httpProxy.GET(delivery.URLPrefix+"/*any", func(c *gin.Context) {
+		if lf.Exists(delivery.URLPrefix, c.Request.URL.Path) {
+			fileserver.ServeHTTP(c.Writer, c.Request)
+			c.Abort()
+		}
+	})
 }
